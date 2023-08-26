9c009ee8409559d4f922e0b2cfef99a8450ab12f
Vindicatree <119718565+Vindicatree@users.noreply.github.com>
2023-08-26 16:44:56 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const header = document.querySelector("header");
 const sectionOne = document.querySelector(".hero");
 
 const sectionOneOptions = {
-    rootMargin: "-300px 0px 0px 0px"
+    rootMargin: "-500px 0px 0px 0px"
 };
 
 const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver) {
