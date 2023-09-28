4fe2fc7fe1ffe598b8df5bc8fe85e7d0198695a3
Vindicatree <119718565+Vindicatree@users.noreply.github.com>
2023-09-28 13:44:03 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -58,3 +58,45 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 }
+
+/*carousel*/
+
+const slider = document.querySelector('.slider');
+const carousel = document.querySelector('.carousel');
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+var direction;
+
+prev.addEventListener('click', function() {
+    if (direction === -1) {
+        slider.appendChild(slider.firstElementChild);
+        direction = 1;
+    }
+    
+    direction = 1;
+    carousel.style.justifyContent = 'flex-end';
+    slider.style.transform = 'translate(33.333%)';
+});
+
+next.addEventListener('click', function() {
+    direction = -1;
+    carousel.style.justifyContent = 'flex-start';
+    slider.style.transform = 'translate(-33.333%)';
+});
+
+slider.addEventListener('transitionend', function() {
+    if (direction === -1) {
+        slider.appendChild(slider.firstElementChild);
+    } else if (direction === 1) {
+        slider.prepend(slider.lastElementChild);
+    }
+
+    slider.style.transition = 'none';
+    slider.style.transform = 'translate(0)';
+    setTimeout(function() {
+        slider.style.transition = 'all 0.5s';
+    })
+
+})
